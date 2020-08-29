f28e68b90c9eb025d7e1863c6804b0f19c0fa9b4
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-08-29 17:09:40 -0300

    Adding IBlogRepository abstraction with repositories methods definition

---
+++ src/repositories/IBlogPostsRepository.ts
@@ -0,0 +1,7 @@
+import { BlogPost } from "../entities/BlogPost";
+
+export interface IBlogPostsRepository {
+    get(id: string): Promise<BlogPost>;
+    index(): Promise<BlogPost[]>;
+    save(post: BlogPost): Promise<void>;
+}
